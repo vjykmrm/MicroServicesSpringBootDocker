b333e03c4bfa26423927967e22ba284a08e0a12c
Vijay Kumar <vijay.marothi@gmail.com>
2018-11-02 17:42:09 +0530

    Initial Commit

---
+++ gateway-service/src/main/java/pl/piomin/microservices/edge/Application.java
@@ -0,0 +1,20 @@
+package pl.piomin.microservices.edge;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableZuulProxy
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(Application.class).web(true).run(args);
+	}
+
+
+	
+}
